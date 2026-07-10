7dfea1ea7c98bc28176525a798772d31bc125620
Aurora <officialbubbletanks@gmail.com>
2026-07-10 12:32:12 -0400

    cards!

---
+++ BorealisCards2Code/Cards/Defect/HighVoltage.cs
@@ -0,0 +1,34 @@
+﻿using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.Models.Orbs;
+using MegaCrit.Sts2.Core.ValueProps;
+using Void = MegaCrit.Sts2.Core.Models.Cards.Void;
+
+namespace BorealisCards2.BorealisCards2Code.Cards.Defect;
+
+
+[Pool(typeof(DefectCardPool))]
+public class HighVoltage() : BorealisCards2Card(0,
+    CardType.Attack, CardRarity.Common,
+    TargetType.AnyEnemy)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new DamageVar(10M, ValueProp.Move)];
+
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        await CreatureCmd.Damage(choiceContext, play.Target, DynamicVars.Damage, this, play);
+        await OrbCmd.Channel<LightningOrb>(choiceContext, Owner);
+        await OrbCmd.Channel<LightningOrb>(choiceContext, Owner);
+        await OrbCmd.Channel<LightningOrb>(choiceContext, Owner);
+        if(IsUpgraded)
+            await OrbCmd.Channel<LightningOrb>(choiceContext, Owner);
+    }
+
+    protected override void OnUpgrade() => DynamicVars.Damage.UpgradeValueBy(3M);
+}
---
+++ BorealisCards2Code/Cards/Defect/TemperatureShock.cs
@@ -0,0 +1,34 @@
+﻿using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.Models.Cards;
+using MegaCrit.Sts2.Core.Models.Orbs;
+using MegaCrit.Sts2.Core.ValueProps;
+using Void = MegaCrit.Sts2.Core.Models.Cards.Void;
+
+namespace BorealisCards2.BorealisCards2Code.Cards.Defect;
+
+
+
+[Pool(typeof(DefectCardPool))]
+public class TemperatureShock() : BorealisCards2Card(0,
+    CardType.Skill, CardRarity.Common,
+    TargetType.Self)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [];
+    
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Exhaust];
+
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        await OrbCmd.Channel<LightningOrb>(choiceContext, Owner);
+        await OrbCmd.Channel<FrostOrb>(choiceContext, Owner);
+    }
+
+    protected override void OnUpgrade() => RemoveKeyword(CardKeyword.Exhaust);
+}
