6c1e08e26431afa1b71f6df07cc7f2b692624625
Aurora <officialbubbletanks@gmail.com>
2026-07-10 04:02:04 -0400

    new cards pogchamn

---
+++ BorealisCards2Code/Cards/Defect/Nuclear.cs
@@ -0,0 +1,34 @@
+using BaseLib.Utils;
+using BorealisCards2.BorealisCards2Code.Powers.Defect;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.Models.Cards;
+using MegaCrit.Sts2.Core.Models.Orbs;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace BorealisCards2.BorealisCards2Code.Cards.Defect;
+
+
+
+[Pool(typeof(DefectCardPool))]
+public class Nuclear() : BorealisCards2Card(1,
+    CardType.Skill, CardRarity.Rare,
+    TargetType.Self)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new PowerVar<NuclearPower>(2M)];
+    
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.Static(StaticHoverTip.Channeling), HoverTipFactory.FromOrb<PlasmaOrb>()];
+
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        if (IsUpgraded)
+            await OrbCmd.Channel<PlasmaOrb>(new ThrowingPlayerChoiceContext(), Owner);
+        await PowerCmd.Apply<NuclearPower>(choiceContext, Owner.Creature, DynamicVars["NuclearPower"].BaseValue, Owner.Creature, this);
+    }
+}
---
+++ BorealisCards2Code/Cards/Defect/Ripsaw.cs
@@ -0,0 +1,36 @@
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.Models.Cards;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace BorealisCards2.BorealisCards2Code.Cards.Defect;
+
+
+[Pool(typeof(DefectCardPool))]
+public class Ripsaw() : BorealisCards2Card(2,
+    CardType.Attack, CardRarity.Rare,
+    TargetType.AllEnemies)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new DamageVar(14, ValueProp.Move), new CardsVar(6), new RepeatVar(2)];
+    
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromCard<Dazed>()];
+
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        await DamageCmd.Attack(DynamicVars.Damage.BaseValue).WithHitCount(DynamicVars.Repeat.IntValue).FromCard(this, play).TargetingAllOpponents(CombatState).WithHitFx("vfx/vfx_attack_slash").Execute(choiceContext);
+        for (int i = 0; i < DynamicVars.Cards._baseValue; ++i)
+            CardCmd.PreviewCardPileAdd(await CardPileCmd.AddGeneratedCardToCombat(CombatState.CreateCard<Dazed>(Owner), PileType.Discard, Owner));
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Damage.UpgradeValueBy(3M);
+    }
+}
