6042ffcf7451418c3109a07beb3f05e3e393c542
Aurora <officialbubbletanks@gmail.com>
2026-07-10 21:10:24 -0400

    wawa :3

---
+++ BorealisCards2Code/Cards/Ironclad/Berserker.cs
@@ -0,0 +1,28 @@
+using BaseLib.Utils;
+using BorealisCards2.BorealisCards2Code.Cards;
+using BorealisCards2.BorealisCards2Code.Powers.Defect;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.CardPools;
+
+namespace BorealisCards2.BorealisCards2Code.Cards.Ironclad;
+
+[Pool(typeof(IroncladCardPool))]
+public class Berserker() : BorealisCards2Card(3,
+    CardType.Power, CardRarity.Rare,
+    TargetType.Self)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new PowerVar<BerserkerPower>(1M)];
+
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        await CreatureCmd.TriggerAnim(Owner.Creature, "PowerUp", Owner.Character.PowerUpAnimDelay);
+        await PowerCmd.Apply<BerserkerPower>(choiceContext, Owner.Creature, DynamicVars["BerserkerPower"].BaseValue, Owner.Creature, this);
+    }
+
+    protected override void OnUpgrade() => EnergyCost.UpgradeBy(-1);
+}
---
+++ BorealisCards2Code/Powers/Ironclad/BerserkerPower.cs
@@ -0,0 +1,41 @@
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.Extensions;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Orbs;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace BorealisCards2.BorealisCards2Code.Powers.Defect;
+
+
+public sealed class BerserkerPower : BorealisCards2Power
+{
+    public override PowerType Type => PowerType.Buff;
+
+    public override PowerStackType StackType => PowerStackType.Counter;
+
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.Static(StaticHoverTip.Channeling), HoverTipFactory.FromOrb<LightningOrb>()];
+
+    public override async Task AfterDamageReceived(
+        PlayerChoiceContext choiceContext,
+        Creature target,
+        DamageResult result,
+        ValueProp props,
+        Creature? dealer,
+        CardModel? cardSource)
+    {
+        if (target != Owner || result.UnblockedDamage <= 0 || Owner.CombatState.CurrentSide != Owner.Side)
+            return;
+        for (int i = 0; i < Amount; i++)
+        {
+            CardModel card2 = PileType.Draw.GetPile(Owner.Player).Cards.Where( c => c.Type == CardType.Attack && !c.Keywords.Contains(CardKeyword.Unplayable)).ToList().StableShuffle(Owner.Player.RunState.Rng.Shuffle).FirstOrDefault() ?? PileType.Draw.GetPile(Owner.Player).Cards.ToList().StableShuffle(Owner.Player.RunState.Rng.Shuffle).FirstOrDefault();
+            if (card2 == null)
+                return;
+            await CardCmd.AutoPlay(choiceContext, card2, null);   
+        }
+    }
+}
