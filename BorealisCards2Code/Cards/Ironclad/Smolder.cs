f1b81aa32273adafed365b71b628247c83a5f0b6
Aurora <officialbubbletanks@gmail.com>
2026-07-13 01:30:11 -0400

    boom shakalaka

---
+++ BorealisCards2Code/Cards/Ironclad/Smolder.cs
@@ -0,0 +1,42 @@
+﻿using BaseLib.Utils;
+using MegaCrit.Sts2.Core.CardSelection;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.Models.Powers;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace BorealisCards2.BorealisCards2Code.Cards.Ironclad;
+
+
+[Pool(typeof(IroncladCardPool))]
+public class Smolder() : BorealisCards2Card(0,
+    CardType.Skill, CardRarity.Common,
+    TargetType.AnyEnemy)
+{
+    public override bool GainsBlock => true;
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new PowerVar<VulnerablePower>(1M)];
+    
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        await PowerCmd.Apply<VulnerablePower>(choiceContext, play.Target, 1, Owner.Creature, this);
+        CardModel card = (await CardSelectCmd.FromHand(choiceContext, Owner, new CardSelectorPrefs(CardSelectorPrefs.ExhaustSelectionPrompt, 1), null, this)).FirstOrDefault();
+        if (card != null) {
+            if (card.Type == CardType.Attack)
+            {
+                await PowerCmd.Apply<VulnerablePower>(choiceContext, play.Target, DynamicVars.Vulnerable._baseValue, Owner.Creature, this);   
+            }
+            await CardCmd.Exhaust(choiceContext, card);
+        }
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars["VulnerablePower"].UpgradeValueBy(1M);
+    }
+}
