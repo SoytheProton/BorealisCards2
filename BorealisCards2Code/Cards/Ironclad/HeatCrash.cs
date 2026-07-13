502ffefa98d8c786ac3076a312ef317e0565807b
SoytheProton <alttimelinemod@gmail.com>
2026-07-12 22:31:21 -0700

    heatbash

--- BorealisCards2Code/Cards/Ironclad/HeatCrash.cs
+++ BorealisCards2Code/Cards/Ironclad/HeatCrash.cs
@@ -2,8 +2,10 @@ using BaseLib.Utils;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.Models.Powers;
 using MegaCrit.Sts2.Core.ValueProps;
 
 namespace BorealisCards2.BorealisCards2Code.Cards.Ironclad;
@@ -13,7 +15,9 @@ public class HeatCrash() : BorealisCards2Card(2,
     CardType.Attack, CardRarity.Rare,
     TargetType.AnyEnemy)
 {
-    protected override IEnumerable<DynamicVar> CanonicalVars => [new DamageVar(13M, ValueProp.Move), new CardsVar(3)];
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new DamageVar(18M, ValueProp.Move)];
+    
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromPower<VulnerablePower>(), HoverTipFactory.FromPower<ArtifactPower>()];
 
     protected override async Task OnPlay(
         PlayerChoiceContext choiceContext,
@@ -23,19 +27,14 @@ public class HeatCrash() : BorealisCards2Card(2,
             .WithAttackerAnim(MegaCrit.Sts2.Core.Models.Characters.Ironclad.GetHeavyAnimIfApplicable(Owner.Character), MegaCrit.Sts2.Core.Models.Characters.Ironclad.GetHeavyAttackDelayIfApplicable(Owner.Character)).
             WithHitFx("vfx/vfx_heavy_blunt", tmpSfx: "heavy_attack.mp3").
             WithHitVfxSpawnedAtBase().Execute(choiceContext);
-        var cards = await CardPileCmd.Draw(choiceContext, DynamicVars.Cards.BaseValue, Owner);
-        foreach (var card in cards)
-        {
-            if (card.Type == CardType.Attack && card.CostsEnergyOrStars(true))
-            {
-                card.EnergyCost.SetUntilPlayed(0);
-            }
-        }
+        foreach (var hittableEnemy in CombatState.HittableEnemies)
+            if (hittableEnemy.HasPower<ArtifactPower>())
+                await PowerCmd.Remove<ArtifactPower>(hittableEnemy);
+        await PowerCmd.Apply<VulnerablePower>(choiceContext, CombatState.HittableEnemies, DynamicVars.Vulnerable.BaseValue, Owner.Creature, this);
     }
 
     protected override void OnUpgrade()
     {
         DynamicVars.Damage.UpgradeValueBy(4M);
-        DynamicVars.Cards.UpgradeValueBy(1);
     }
 }
