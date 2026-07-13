1bb2169aa6232ba403e17512d61a6b09af3fa6b2
Aurora <officialbubbletanks@gmail.com>
2026-07-12 22:35:21 -0400

    bloody strike

---
+++ BorealisCards2Code/Cards/Ironclad/BloodyStrike.cs
@@ -0,0 +1,33 @@
+using BaseLib.Utils;
+using BorealisCards2.BorealisCards2Code.Cards;
+using BorealisCards2.BorealisCards2Code.Powers.Defect;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace BorealisCards2.BorealisCards2Code.Cards.Ironclad;
+
+[Pool(typeof(IroncladCardPool))]
+public class BloodyStrike() : BorealisCards2Card(1,
+    CardType.Power, CardRarity.Uncommon,
+    TargetType.AnyEnemy)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new DamageVar(17M, ValueProp.Move), new PowerVar<BloodyStrikePower>(1M)];
+
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        await CreatureCmd.TriggerAnim(Owner.Creature, "Attack", Owner.Character.AttackAnimDelay);
+        await CreatureCmd.Damage(choiceContext, play.Target, DynamicVars.Damage, this, play);
+        await PowerCmd.Apply<BloodyStrikePower>(choiceContext, Owner.Creature, DynamicVars["BloodyStrikePower"].BaseValue, Owner.Creature, this);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Damage.UpgradeValueBy(6M);
+    }
+}
---
+++ BorealisCards2Code/Powers/Ironclad/BloodyStrikePower.cs
@@ -0,0 +1,30 @@
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.Extensions;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Orbs;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace BorealisCards2.BorealisCards2Code.Powers.Defect;
+
+
+public sealed class BloodyStrikePower : BorealisCards2Power
+{
+    public override PowerType Type => PowerType.Buff;
+
+    public override PowerStackType StackType => PowerStackType.Counter;
+
+    public override async Task AfterPlayerTurnStart(PlayerChoiceContext choiceContext, Player player)
+    {
+        if (player != Owner.Player)
+            return;
+        Flash();
+        await CreatureCmd.Damage(choiceContext, Owner, Amount, ValueProp.Unpowered, Owner);
+    }
+}
