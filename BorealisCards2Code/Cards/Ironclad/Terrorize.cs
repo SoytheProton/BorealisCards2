1dca11e8e507e3d6ca55fc219c5d9a1072edf0aa
Aurora <officialbubbletanks@gmail.com>
2026-07-13 01:39:37 -0400

    terrorize

---
+++ BorealisCards2Code/Cards/Ironclad/Terrorize.cs
@@ -0,0 +1,34 @@
+﻿using BaseLib.Utils;
+using MegaCrit.Sts2.Core.CardSelection;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.Models.Powers;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace BorealisCards2.BorealisCards2Code.Cards.Ironclad;
+
+[Pool(typeof(IroncladCardPool))]
+public class Terrorize() : BorealisCards2Card(1,
+    CardType.Skill, CardRarity.Uncommon,
+    TargetType.AnyEnemy)
+{
+    public override bool GainsBlock => true;
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new PowerVar<VulnerablePower>(5M)];
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Exhaust, CardKeyword.Innate];
+    
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        await PowerCmd.Apply<VulnerablePower>(choiceContext, play.Target, DynamicVars.Vulnerable._baseValue, Owner.Creature, this);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars["VulnerablePower"].UpgradeValueBy(1M);
+    }
+}
