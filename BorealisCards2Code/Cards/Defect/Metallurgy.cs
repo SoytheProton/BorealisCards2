6cd811cdf2effc4e97b7f9e30587135cba83a191
SoytheProton <alttimelinemod@gmail.com>
2026-07-16 01:06:06 -0700

    metbaburby

--- BorealisCards2Code/Cards/Defect/Metallurgy.cs
+++ BorealisCards2Code/Cards/Defect/Metallurgy.cs
@@ -2,9 +2,13 @@
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.Models.Cards;
+using MegaCrit.Sts2.Core.Models.Orbs;
+using MegaCrit.Sts2.Core.Models.Powers;
 using MegaCrit.Sts2.Core.ValueProps;
 using Void = MegaCrit.Sts2.Core.Models.Cards.Void;
 
@@ -15,53 +19,29 @@ public sealed class Metallurgy() : BorealisCards2Card(1,
     CardType.Attack, CardRarity.Uncommon,
     TargetType.AnyEnemy)
 {
-    private decimal _extraDamageFromPlays;
-    
-    protected override IEnumerable<DynamicVar> CanonicalVars => [new DamageVar(6M, ValueProp.Move), new ("Increase", 6M)];
-    public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Retain];
-
-    private decimal ExtraDamageFromPlays
-    {
-        get => _extraDamageFromPlays;
-        set
-        {
-            AssertMutable();
-            _extraDamageFromPlays = value;
-        }
-    }
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new DamageVar(11M, ValueProp.Move), new PowerVar<VulnerablePower>(2)];
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromPower<VulnerablePower>(), HoverTipFactory.FromCard<Burn>()];
     
     protected override async Task OnPlay(
         PlayerChoiceContext choiceContext,
         CardPlay play)
     {
         await DamageCmd.Attack(DynamicVars.Damage.BaseValue).FromCard(this, play).Targeting(play.Target).WithHitFx("vfx/vfx_attack_slash").Execute(choiceContext);
-        CardCmd.PreviewCardPileAdd(await CardPileCmd.AddGeneratedCardToCombat(CombatState.CreateCard<Void>(Owner), PileType.Draw, Owner));
+        var amount = DynamicVars["Power"].IntValue;
+        await PowerCmd.Apply<WeakPower>(choiceContext, play.Target, amount, Owner.Creature, this);
+        await PowerCmd.Apply<VulnerablePower>(choiceContext, play.Target, amount, Owner.Creature, this);
+    }
+
+    public override async Task AfterCardDrawn(
+        PlayerChoiceContext choiceContext,
+        CardModel card,
+        bool fromHandDraw)
+    {
+        if (card != this)
+            return;
+        CardCmd.PreviewCardPileAdd(await CardPileCmd.AddGeneratedCardToCombat(CombatState.CreateCard<Burn>(Owner), PileType.Discard, Owner));
         await Cmd.Wait(0.5f);
     }
 
-    protected override void OnUpgrade()
-    {
-        DynamicVars.Damage.UpgradeValueBy(2M);
-        DynamicVars["Increase"].UpgradeValueBy(2M);
-    }
-
-    public override async Task AfterCardExhausted(
-        PlayerChoiceContext choiceContext,
-        CardModel card,
-        bool _)
-    {
-        if (card.Owner != Owner || card.Type != CardType.Status)
-            return;
-        DamageVar damage = DynamicVars.Damage;
-        damage.BaseValue += DynamicVars["Increase"].BaseValue;
-        ExtraDamageFromPlays += DynamicVars["Increase"].BaseValue;
-        await CardPileCmd.Add(this, PileType.Hand);
-    }
-    
-    protected override void AfterDowngraded()
-    {
-        base.AfterDowngraded();
-        DamageVar damage = DynamicVars.Damage;
-        damage.BaseValue += ExtraDamageFromPlays;
-    }
+    protected override void OnUpgrade() => DynamicVars.Vulnerable.UpgradeValueBy(1);
 }
