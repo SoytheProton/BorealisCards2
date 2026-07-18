7da353b1a2fba58cd4a87a192671d8775bb5aec6
Aurora <officialbubbletanks@gmail.com>
2026-07-18 01:43:08 -0400

    soopanatuwa

---
+++ BorealisCards2Code/Cards/Necrobinder/Supernatural.cs
@@ -0,0 +1,45 @@
+using BaseLib.Utils;
+using BorealisCards2.BorealisCards2Code.Cards;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.Models.Cards;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace BorealisCards2.BorealisCards2Code.Cards.Necrobinder;
+
+[Pool(typeof(NecrobinderCardPool))]
+public class Supernatural() : BorealisCards2Card(1,
+    CardType.Skill, CardRarity.Rare,
+    TargetType.Self)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new SummonVar(9)];
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Ethereal];
+
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        await CreatureCmd.TriggerAnim(Owner.Creature, MegaCrit.Sts2.Core.Models.Characters.Necrobinder.GetSummonAnimIfApplicable(Owner.Character), MegaCrit.Sts2.Core.Models.Characters.Necrobinder.GetSummonDelayIfApplicable(Owner.Character));
+        await OstyCmd.Summon(choiceContext, Owner, DynamicVars.Summon.BaseValue, this);
+    }
+    
+    public override async Task AfterAutoPostPlayPhaseEntered(
+        PlayerChoiceContext choiceContext,
+        Player player)
+    {
+        CardPile pile = Pile;
+        if ((pile != null ? (pile.Type != PileType.Exhaust ? 1 : 0) : 1) != 0 || player != Owner)
+            return;
+        await CardCmd.AutoPlay(choiceContext, this, null);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Summon.UpgradeValueBy(2M);
+    }
+}
